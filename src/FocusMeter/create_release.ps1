661f85167664a0f0cc1e7ed77af37b0d1455dc4d
blackened <blackened>
2012-11-04 22:58:39 +0200

    Released 17 build.

--- src/FocusMeter/create_release.ps1
+++ src/FocusMeter/create_release.ps1
@@ -8,7 +8,7 @@ rd "$publishDir" -recurse -force -ErrorAction SilentlyContinue | out-null
 mkdir "$publishDir" -ErrorAction SilentlyContinue | out-null
 
 # Publish
-$version = 16
+$version = 17
 & $msbuildExe /target:publish /p:Configuration=Release /p:PublishDir="$publishDir" /property:ApplicationRevision=$version
 if ($lastexitcode -ne 0) { throw }
 
